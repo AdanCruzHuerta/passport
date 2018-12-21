eb5a1bf0795c02f1a72e4393e5b243b22090a20d
Adan Cruz <acruzchoferpro.com>
2018-12-21 15:43:07 +0000

    primer commit, mostrando custom event, mandado eventos de componente hijo a padre

---
+++ resources/assets/js/components/ChildComponent.vue
@@ -0,0 +1,33 @@
+<!--
+Para emitir evento es importante definir el nombre del evento en nomenclatura minusculas y separado por guiones
+por ejemplo 'my-even'
+-->
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Componente Hijo</div>
+                    <div class="card-body">
+                        <button class="btn btn-primary" @click="$emit('my-event')">Ocultar mensaje</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ChildComponent",
+        methods: {
+            //
+        }
+    }
+</script>
+
+<style scoped>
+    .container{
+        margin-top: 10px !important;
+    }
+</style>
---
+++ resources/assets/js/components/ExampleComponent.vue
@@ -0,0 +1,43 @@
+<!--
+Para recibir eventos se debe hacer referencia al nombre del evento en el componente en el que se genero el evento
+<child-component @my-event="getMessage"></child-component>
+-->
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Componente padre</div>
+                    <div class="card-body">
+                        <label class="text-success text-center" v-show="mostrarMensaje == true">Este es mi mensaje en el componente padre</label>
+                        <child-component @my-event="getMessage"></child-component>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data() {
+            return {
+                mostrarMensaje: true
+            }
+        },
+        methods: {
+            getMessage() {
+                this.mostrarMensaje = !this.mostrarMensaje;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .container{
+        margin-top: 25px !important;
+    }
+</style>
